f3ec94b9079aa6612834a3b93aba5e05d2d2aa95
Will Fleming <wfleming77@gmail.com>
2015-05-31 22:04:13 -0700

    Implement encode method and tests

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,7 +1,44 @@
 (ns alphabet-cipher.coder)
 
+(defn rotate [x coll]
+  (cond
+    (> x (count coll)) (rotate (- x (count coll)) coll)
+    (pos? x) (into (vec (drop x coll)) (take x coll))
+    (neg? x) (rotate (+ (count coll) x) coll)
+    :else coll))
+
+(defn alphabet-list []
+  (map char (range 97 123)))
+
+(defn letter-position [x]
+  (.indexOf (map char (range 97 123)) x))
+
+(defn substition-chart []
+  (let [al (alphabet-list)]
+    (map #(rotate % al) (range 26))))
+
+(defn keyword-for-message [keyword message]
+  (let [num-repeats (inc (int (/ (count message) (count keyword))))
+        repeated-keyword (apply str (repeat num-repeats keyword))]
+    (take (count message) repeated-keyword)))
+
+(defn encode-letter [message-letter keyword-letter]
+  (let [chart (substition-chart)
+        row (nth chart (letter-position message-letter))]
+  (nth row (letter-position keyword-letter))))
+
+(defn encode-loop [keyword message]
+  (loop [kw (keyword-for-message keyword message)
+         msg message
+         acc []]
+    (if (empty? msg)
+      acc
+      (recur (rest kw) (rest msg) (conj acc (encode-letter (first msg) (first kw)))))))
+
 (defn encode [keyword message]
-  "encodeme")
+  (let [kw (encode-loop keyword message)]
+    (apply str kw)))
+
 
 (defn decode [keyword message]
   "decodeme")
--- alphabet-cipher/test/alphabet_cipher/coder_test.clj
+++ alphabet-cipher/test/alphabet_cipher/coder_test.clj
@@ -2,6 +2,27 @@
   (:require [clojure.test :refer :all]
             [alphabet-cipher.coder :refer :all]))
 
+(deftest test-letter-position
+  (testing "retrieve the correct position"
+    (is (= 0 (letter-position \a)))
+    (is (= 1 (letter-position \b)))
+    (is (= 2 (letter-position \c)))))
+
+(deftest test-alphabet-list
+  (testing "creates a list with all letters of the alphabet"
+    (is (= '(\a \b \c \d \e \f \g \h \i \j \k \l \m \n \o \p \q \r \s \t \u \v \w \x \y \z)
+           (alphabet-list)))))
+
+(deftest test-keyword-for-message
+  (testing "creates a properly length keyword"
+    (is (= '(\s \c \o \n \e \s \s \c \o \n \e \s \s \c \o)
+           (keyword-for-message "scones" "meetmebythetree")))))
+
+(deftest test-encode-letter
+  (testing "encodes a letter"
+    (is (= \e (encode-letter \m \s)))))
+
+
 (deftest test-encode
   (testing "can encode give a secret keyword"
     (is (= "hmkbxebpxpmyllyrxiiqtoltfgzzv"
