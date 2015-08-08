7e99cae056db29c10d5cf65665ef302c9525bb35
Will Fleming <wfleming77@gmail.com>
2015-08-08 14:12:25 -0700

    wonderland solution

--- wonderland-number/src/wonderland_number/finder.clj
+++ wonderland-number/src/wonderland_number/finder.clj
@@ -1,5 +1,7 @@
 (ns wonderland-number.finder)
 
 (defn wonderland-number []
-  ;; calculate me
-  42)
+  (let [nums (range (int 100000) (int 1000000))
+        digits (fn [x] (set (str x)))
+        is-valid? (fn [x i] (= (digits x) (digits (* x i))))]
+    (first (filter #(every? identity (map (partial is-valid? %) (range 2 7))) nums))))
